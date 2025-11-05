6ac3351a476781c105c892baa33c3de17da05ebd
HarshTheSharma <harshthesharma@gmail.com>
2025-11-05 11:47:33 -0800

    added mixed case tests to PasswordTest

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,31 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, two_leading_letters)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aab");
+	ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, repeated_leading_character)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaab");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, has_mixed_case_true)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aB");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, has_mixed_case_false)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("aa");
+	ASSERT_FALSE(actual);
+}
